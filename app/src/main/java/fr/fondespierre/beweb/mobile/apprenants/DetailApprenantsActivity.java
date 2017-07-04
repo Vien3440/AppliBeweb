cde4db8b546df2c039bb36b1ae0e29e8b077f412
vien3440 <viendev@gmail.com>
2017-07-04 22:26:03 +0200

    Création des layout

---
+++ app/src/main/java/fr/fondespierre/beweb/mobile/apprenants/DetailApprenantsActivity.java
@@ -0,0 +1,13 @@
+package fr.fondespierre.beweb.mobile.apprenants;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DetailApprenantsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.detail_apprenants);
+    }
+}
---
+++ app/src/main/java/fr/fondespierre/beweb/mobile/apprenants/ListeApprenantsActivity.java
@@ -0,0 +1,13 @@
+package fr.fondespierre.beweb.mobile.apprenants;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ListeApprenantsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.liste_apprenants);
+    }
+}
